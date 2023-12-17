87f7f62af50e517406057301e90afb9f72c86569
Maheer Sayeed <maheersayeed@gmail.com>
2023-12-16 19:59:54 -0500

    fixed touch issues

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,33 +1,67 @@
 import '../stylesheets/project.css';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 
 const Menu = () => {
     const navigate = useNavigate();
 
-    const navigateToPage = (route,id) => {
+    const location = useLocation()
+
+    const navigateToPage = (route) => {
+        // const buttons = document.querySelectorAll('.control');
+        // buttons.forEach((button) => {
+        // button.classList.remove('active-btn');
+        // });
+
+        // const current_button = document.querySelector(`[data-id="${id}"]`);
+        // current_button.classList.add('active-btn');
+        navigate(route);
+    };
+
+    const refresh = useEffect(()=>{
+        
+
         const buttons = document.querySelectorAll('.control');
         buttons.forEach((button) => {
         button.classList.remove('active-btn');
         });
 
-        const current_button = document.querySelector(`[data-id="${id}"]`);
-        current_button.classList.add('active-btn');
-        navigate(route);
-    };
+        if (location.pathname === "/"){
+            const current_button = document.querySelector(`[data-id="projects"]`);
+            current_button.classList.add('active-btn');
+            return;
+        }
+
+        for (let button of buttons){
+            const current_id = button.getAttribute("data-id")
+            if (location.pathname == `/${current_id}`){
+                const current_button = document.querySelector(`[data-id="${current_id}"]`);
+                current_button.classList.add('active-btn');
+                return;
+            }
+        }
+
+        
+
+
+
+
+
+    })
     
     return(
 
         <div class="controls" data-id="controls">
 
-            <div class="control control-2 active-btn" data-id="projects"
-            onClick={()=>{navigateToPage("/","projects")}}>    
+            <div class="control control-2" data-id="projects"
+            onClick={()=>{navigateToPage("/")}}>    
                 <i class="fa-solid fa-diagram-project"></i>
             </div>
 
-            <div class="control control-3" data-id="about"
-            onClick={()=>{navigateToPage("/resume","about")}}>
+            <div class="control control-3" data-id="resume"
+            onClick={()=>{navigateToPage("/resume")}}>
                 <i class="fa-solid fa-file-lines"></i>
             </div>
         </div>
--- src/components/sliding_image.js
+++ src/components/sliding_image.js
@@ -9,6 +9,7 @@ const Sliding_Image = (props) => {
     const dragSlider = (event) => {
         
         const mouseX = event.clientX
+        
         const element = document.getElementById(props.image_element_id)
         const rect = element.getBoundingClientRect();
         const leftBound = rect.left;
@@ -25,14 +26,44 @@ const Sliding_Image = (props) => {
          
     }
 
+    const dragSliderTouch = (event) => {
+        
+        const touchX = event.touchX
+
+        const element = document.getElementById(props.image_element_id)
+        const rect = element.getBoundingClientRect();
+        const leftBound = rect.left;
+        const rightBound = rect.right;
+        var percent = (touchX-leftBound)/(rightBound-leftBound)*100
+        if (percent >= 100){
+            percent = 100;
+        }
+        if (percent <= 0){
+            percent = 0;
+        }
+        console.log(percent)
+        setSliderShow(percent)
+         
+    }
+
     const handleMouseDown = (e) => {
         document.addEventListener('mousemove', dragSlider);
         document.addEventListener('mouseup', handleMouseUp);
     };
 
-    const handleMouseUp = () => {
+    const handleTouchDown = (e) => {
+        document.addEventListener('touchmove', dragSliderTouch);
+        document.addEventListener('touchstart', handleTouchUp);
+    };
+
+    const handleMouseUp = (e) => {
         document.removeEventListener('mousemove', dragSlider);
         document.removeEventListener('mouseup', handleMouseUp);
+    };
+
+    const handleTouchUp = (e) => {
+        document.removeEventListener('touchmove', dragSliderTouch);
+        document.removeEventListener('touchstart', handleTouchUp);
     };
 
     
@@ -40,7 +71,7 @@ const Sliding_Image = (props) => {
 
         <div class="img-comp-container">
             
-            <div class="img-comp-slider" style={{marginLeft:`${sliderShow}%`}} onMouseDown={(event)=>{handleMouseDown(event)}}></div>
+            <div class="img-comp-slider" style={{marginLeft:`${sliderShow}%`}} onTouchStart={(event)=>{handleTouchDown(event)}} onMouseDown={(event)=>{handleMouseDown(event)}}></div>
 
             <div class="img-comp-img" id={props.image_element_id}>
                 <img src={props.image_left}/>
