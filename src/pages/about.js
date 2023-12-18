c7e9bb4e7714e5d0b68f178c6cf5e9158bd70276
Maheer Sayeed <maheersayeed@gmail.com>
2023-12-18 16:47:03 -0500

    updated resume reduce

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,25 @@ import React from 'react';
 import '../stylesheets/project.css';
 import '../components/home_post'
 import { Title } from '../components/title';
+import { useState, useEffect } from 'react';
 
 
 function About_Page() {
 
+    const [zoomVal, setZoomVal] = useState(window.innerWidth);
+    
+    const updateWidth = useEffect(()=> {
+        const width = window.innerWidth;
+        
+        if (width < 600){
+            setZoomVal(50)
+        }
+        else{
+            setZoomVal(100)
+        }
+    })
+
+    
   return (
         <body class = "wholeBody">
             
@@ -17,7 +32,7 @@ function About_Page() {
                     <iframe
                         class = "pdf-frame"
                         title="PDF Viewer"
-                        src="/resume/maheer_resume.pdf" 
+                        src={`/resume/maheer_resume.pdf#zoom=${zoomVal}`}
                     />
                 </div>
 
