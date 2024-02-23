b6427d26a2d7be09569311cb2ba9f50fec55ce8b
Maheer Sayeed <maheersayeed@gmail.com>
2024-02-23 10:08:16 -0500

    changed resume

--- src/utils/convert-resume.js
+++ src/utils/convert-resume.js
@@ -1,22 +1,36 @@
-// const pdfPoppler = require('pdf-poppler');
-const path = require("path")
-
-const pdfFilePath = path.join(__dirname,"../../public/resume/maheer-resume.pdf");
-const outputDir = path.join(__dirname,"../../public/resume/")
-
+const path = require("path");
+const fs = require("fs");
+const { PDFDocument } = require("pdf-lib");
 const { fromPath } = require("pdf2pic");
 
-const options = {
-  density: 100,
-  saveFilename: "untitled",
-  savePath: outputDir,
-  format: "png",
-};
+const pdfFilePath = path.join(__dirname, "../../public/resume/maheer-resume.pdf");
+const outputDir = path.join(__dirname, "../../public/resume/");
 
-const convert = fromPath(pdfFilePath, options);
+// Function to get dimensions of a PDF page
+async function getPageDimensions(pdfPath, pageNumber = 1) {
+    const pdfBytes = await fs.promises.readFile(pdfPath);
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+    const page = pdfDoc.getPage(pageNumber - 1);
+    const { width, height } = page.getSize();
+    return { width, height };
+}
 
-convert(1, { responseType: "image" })
-  .then((resolve) => {
-    console.log("Page 1 is now converted as image");
-    return resolve;
-  });
+(async () => {
+    const { width, height } = await getPageDimensions(pdfFilePath);
+    const options = {
+        density: 1000, // Increased density for higher resolution
+        saveFilename: "untitled",
+        savePath: outputDir,
+        format: "jpg",
+        width,
+        height
+    };
+
+    const convert = fromPath(pdfFilePath, options);
+
+    convert(1, { responseType: "image" })
+        .then((resolve) => {
+            console.log("Page 1 is now converted as image");
+            return resolve;
+        });
+})();
